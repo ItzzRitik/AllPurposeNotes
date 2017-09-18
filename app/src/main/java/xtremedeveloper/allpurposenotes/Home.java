7e981a22e5b84f3e181a0237811712f05c34cd9e
Ritik Srivastava <ritik.space@gmail.com>
2017-09-18 11:43:50 -0700

    Menu

--- app/src/main/java/xtremedeveloper/allpurposenotes/Home.java
+++ app/src/main/java/xtremedeveloper/allpurposenotes/Home.java
@@ -1,16 +1,33 @@
 package xtremedeveloper.allpurposenotes;
 import android.graphics.Color;
+import android.graphics.Typeface;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class Home extends AppCompatActivity
 {
+    FloatingActionButton close_menu;
+    AppBarLayout appbar;
+    TextView display_name;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
         getWindow().setStatusBarColor(Color.TRANSPARENT);
         setContentView(R.layout.activity_home);
+
+        appbar = (AppBarLayout)findViewById(R.id.appbar);
+        close_menu=(FloatingActionButton)findViewById(R.id.close_menu);
+        close_menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {appbar.setExpanded(false);}
+        });
+
+        display_name=(TextView)findViewById(R.id.display_name);
+        display_name.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/vdub.ttf"));
     }
 }

13be8bb0dc88a86d95eacef1f13fc97fdf595692
Ritik Srivastava <ritik.space@gmail.com>
2017-09-18 10:11:57 -0700

    Menu

--- app/src/main/java/xtremedeveloper/allpurposenotes/Home.java
+++ app/src/main/java/xtremedeveloper/allpurposenotes/Home.java
@@ -1,11 +1,16 @@
 package xtremedeveloper.allpurposenotes;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
 public class Home extends AppCompatActivity
 {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
         setContentView(R.layout.activity_home);
     }
 }

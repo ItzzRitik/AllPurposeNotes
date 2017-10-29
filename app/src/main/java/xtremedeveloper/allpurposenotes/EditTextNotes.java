ca938df28286205e3df3bc70ad774893e898a35f
Ritik Srivastava <ritik.space@gmail.com>
2017-10-30 03:19:06 +0530

    Add Notes

--- app/src/main/java/xtremedeveloper/allpurposenotes/EditTextNotes.java
+++ app/src/main/java/xtremedeveloper/allpurposenotes/EditTextNotes.java
@@ -2,6 +2,7 @@ package xtremedeveloper.allpurposenotes;
 
 import android.animation.Animator;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.ColorStateList;
@@ -9,6 +10,7 @@ import android.graphics.Typeface;
 import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.InputFilter;
@@ -24,6 +26,7 @@ import android.view.animation.AnimationUtils;
 import android.view.animation.DecelerateInterpolator;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
@@ -38,22 +41,39 @@ public class EditTextNotes extends AppCompatActivity {
     EditText windowTitle,textValue;
     FloatingActionButton editNotes;
     RelativeLayout editAnim;
+    ImageView unedit,textSave;
     Intent intent;
     Animation anim;
+    boolean fabPressed=false;
     @Override
-    public void onResume() {
-        super.onResume();
+    public void onBackPressed()
+    {
+        if(textValue.getInputType()!=InputType.TYPE_NULL) {unedit.performClick();}
+        else if(fabPressed)
+        {
+            AlertDialog.Builder builder1 = new AlertDialog.Builder(EditTextNotes.this);
+            builder1.setMessage("Your changes are not saved, How do you want to proceed ?");
+            builder1.setTitle("Discard ?");
+            builder1.setCancelable(true);
+            builder1.setPositiveButton(
+                    "Save",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            save();finish();
+                        }
+                    });
 
-    }
-    @Override
-    public void onPause() {
-        super.onPause();
-        List<String> note_text = new Gson().fromJson(notes.getString("note_text", null), new TypeToken<ArrayList<String>>() {}.getType());
-        note_text.set(intent.getIntExtra("position",0),textValue.getText().toString());
-        notes.edit().putString("note_text", new Gson().toJson(note_text)).apply();
-        note_text = new Gson().fromJson(notes.getString("note_title", null), new TypeToken<ArrayList<String>>() {}.getType());
-        note_text.set(intent.getIntExtra("position",0),windowTitle.getText().toString());
-        notes.edit().putString("note_title", new Gson().toJson(note_text)).apply();
+            builder1.setNegativeButton(
+                    "Don't Save",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            finish();
+                        }
+                    });
+            AlertDialog alert11 = builder1.create();
+            alert11.show();
+        }
+        else {finish();}
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -102,18 +122,18 @@ public class EditTextNotes extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 int cy=(int)(editNotes.getY() + editNotes.getHeight() / 2);
-                int cx=(int)(editNotes.getX() + editNotes.getWidth() / 2);
+                int cx=(int)(editNotes.getX() + editNotes.getWidth() / 2);fabPressed=true;
                 Animator animator = ViewAnimationUtils.createCircularReveal(editAnim,cx,cy, 0,textValue.getHeight()*141/100);
                 animator.setInterpolator(new DecelerateInterpolator());animator.setDuration(500);
                 animator.addListener(new Animator.AnimatorListener() {
                     @Override
-                    public void onAnimationStart(Animator animation) {editAnim.setVisibility(View.VISIBLE);
-                        anim = AnimationUtils.loadAnimation(EditTextNotes.this, R.anim.fade_out);editNotes.setVisibility(View.GONE);
+                    public void onAnimationStart(Animator animation) {editAnim.setVisibility(View.VISIBLE);editNotes.setVisibility(View.GONE);
+                        anim = AnimationUtils.loadAnimation(EditTextNotes.this, R.anim.fade_out);anim.setFillAfter(false);
                         new Handler().postDelayed(new Runnable() {@Override public void run() {editAnim.startAnimation(anim);}},200);}
                     @Override
                     public void onAnimationEnd(Animator animation)
                     {
-                        editAnim.setVisibility(View.GONE);
+                        editAnim.setVisibility(View.GONE);textSave.setVisibility(View.VISIBLE);unedit.setVisibility(View.VISIBLE);
                         textValue.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_FLAG_MULTI_LINE);
                         windowTitle.setInputType(InputType.TYPE_CLASS_TEXT);
                         textValue.requestFocus();showKeyboard(textValue,true);
@@ -128,6 +148,31 @@ public class EditTextNotes extends AppCompatActivity {
             }
         });
 
+        unedit=customNav.findViewById(R.id.unedit);
+        unedit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textSave.setVisibility(View.GONE);unedit.setVisibility(View.GONE);
+                textValue.setInputType(InputType.TYPE_NULL);windowTitle.setInputType(InputType.TYPE_NULL);
+                editNotes.setVisibility(View.VISIBLE);showKeyboard(textValue,false);
+            }
+        });
+        textSave=customNav.findViewById(R.id.textSave);
+        textSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                save();showKeyboard(textValue,false);finish();
+            }
+        });
+    }
+    public void save()
+    {
+        List<String> note_text = new Gson().fromJson(notes.getString("note_text", null), new TypeToken<ArrayList<String>>() {}.getType());
+        note_text.set(intent.getIntExtra("position",0),textValue.getText().toString());
+        notes.edit().putString("note_text", new Gson().toJson(note_text)).apply();
+        note_text = new Gson().fromJson(notes.getString("note_title", null), new TypeToken<ArrayList<String>>() {}.getType());
+        note_text.set(intent.getIntExtra("position",0),windowTitle.getText().toString());
+        notes.edit().putString("note_title", new Gson().toJson(note_text)).apply();
     }
     public void showKeyboard(View view,boolean what)
     {

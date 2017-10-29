7f2eed201d750b72d7d2e8ee44121a10a92234d8
Ritik Srivastava <ritik.space@gmail.com>
2017-10-30 00:13:48 +0530

    Add Notes

--- app/src/main/java/xtremedeveloper/allpurposenotes/EditTextNotes.java
+++ app/src/main/java/xtremedeveloper/allpurposenotes/EditTextNotes.java
@@ -4,13 +4,19 @@ import android.animation.Animator;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.ColorStateList;
+import android.graphics.Typeface;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.InputFilter;
+import android.text.InputType;
 import android.text.Spanned;
 import android.text.method.KeyListener;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewAnimationUtils;
 import android.view.animation.Animation;
@@ -27,10 +33,11 @@ import java.util.List;
 
 public class EditTextNotes extends AppCompatActivity {
     SharedPreferences notes;
-    EditText textValue;
+    EditText windowTitle,textValue;
     FloatingActionButton editNotes;
     RelativeLayout editAnim;
     Intent intent;
+    Animation anim;
     @Override
     public void onResume() {
         super.onResume();
@@ -42,23 +49,39 @@ public class EditTextNotes extends AppCompatActivity {
         List<String> note_text = new Gson().fromJson(notes.getString("note_text", null), new TypeToken<ArrayList<String>>() {}.getType());
         note_text.set(intent.getIntExtra("position",0),textValue.getText().toString());
         notes.edit().putString("note_text", new Gson().toJson(note_text)).apply();
+        note_text = new Gson().fromJson(notes.getString("note_title", null), new TypeToken<ArrayList<String>>() {}.getType());
+        note_text.set(intent.getIntExtra("position",0),windowTitle.getText().toString());
+        notes.edit().putString("note_title", new Gson().toJson(note_text)).apply();
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.edit_text_notes);
         notes = getSharedPreferences("notes", Context.MODE_PRIVATE);
-        getSupportActionBar().setElevation(0);
         intent=getIntent();
-        setTitle(intent.getStringExtra("title"));
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowTitleEnabled(false);
+        actionBar.setDisplayUseLogoEnabled(false);
+        actionBar.setDisplayHomeAsUpEnabled(false);
+        actionBar.setDisplayShowCustomEnabled(true);
+        actionBar.setDisplayShowHomeEnabled(false);
+        actionBar.setElevation(0);
+        ActionBar.LayoutParams lp1 = new ActionBar.LayoutParams(ActionBar.LayoutParams.MATCH_PARENT, ActionBar.LayoutParams.MATCH_PARENT);
+        View customNav = LayoutInflater.from(this).inflate(R.layout.edit_actionbar, null);
+
+        actionBar.setCustomView(customNav, lp1);
+
+
+        windowTitle=customNav.findViewById(R.id.windowTitle);
+        windowTitle.setTypeface(Typeface.createFromAsset(getAssets(), "fonts/exo2.ttf"));
+        windowTitle.setText(intent.getStringExtra("title"));
 
         textValue=findViewById(R.id.textValue);
-        textValue.setTag(textValue.getKeyListener());
-        textValue.setKeyListener(null);
         textValue.setText(intent.getStringExtra("text"));
 
-        editNotes=findViewById(R.id.editNotes);
         editAnim=findViewById(R.id.editAnim);
+        editNotes=findViewById(R.id.editNotes);
         editNotes.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -69,11 +92,12 @@ public class EditTextNotes extends AppCompatActivity {
                 animator.addListener(new Animator.AnimatorListener() {
                     @Override
                     public void onAnimationStart(Animator animation) {editAnim.setVisibility(View.VISIBLE);
-                        Animation anim = AnimationUtils.loadAnimation(EditTextNotes.this, R.anim.fade_out);
-                        anim.setDuration(500);editAnim.startAnimation(anim);editNotes.setVisibility(View.GONE);}
+                        anim = AnimationUtils.loadAnimation(EditTextNotes.this, R.anim.fade_out);editNotes.setVisibility(View.GONE);
+                        new Handler().postDelayed(new Runnable() {@Override public void run() {editAnim.startAnimation(anim);}},200);}
                     @Override
                     public void onAnimationEnd(Animator animation) {editAnim.setVisibility(View.GONE);
-                    textValue.setKeyListener((KeyListener) textValue.getTag());}
+                        textValue.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_FLAG_MULTI_LINE);
+                        windowTitle.setInputType(InputType.TYPE_CLASS_TEXT);}
                     @Override
                     public void onAnimationCancel(Animator animation) {}
                     @Override
